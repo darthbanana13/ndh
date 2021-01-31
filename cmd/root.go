dd6bbbe009d40dfccff157a2b7e9a020bfb201dc
Radu Margineanu <radu.margineanu@scayla.com>
2021-01-31 16:28:49 +0100

    Init cobra

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "ndh",
+	Short: "Get hierarchical dependencies for node packages",
+	Long: "Given a node package name show hierarchical/tree dependencies of the package",
+	RunE: func(_ *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	//TODO: Implement loading .env config
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/darthrevan13/ndh/cmd"
+
+func main() {
+	cmd.Execute()
+}
