97fed59cf24d721fe09118384d9a9abf21d0bd60
Radu Margineanu <radu.margineanu@scayla.com>
2021-01-31 18:11:35 +0100

    Second attempt at calculating flat hierachy

--- pkg/pkgManager/pkgManager.go
+++ pkg/pkgManager/pkgManager.go
@@ -5,18 +5,40 @@ import (
 	"github.com/darthrevan13/ndh/pkg/npmPkg"
 )
 
+type pkgToProcess struct {
+	Name	string
+	Version string
+}
+
 func GetAllDependencies(name, ver string) (map[string]string, error) {
 	flatDependencies := map[string]string{name: ver}
-	for n, v := range flatDependencies {
-		fmt.Println(n)
-		p, _ := npmPkg.GetDependencies(n, v)
-		addMap(flatDependencies, p.Dependencies)
+	pkg := pkgToProcess{
+		Name: name,
+		Version: ver,
+	}
+	pkgsToProcess := []pkgToProcess{pkg}
+
+	for i := 0; i < len(pkgsToProcess); i++ {
+		curPkg := pkgsToProcess[i]
+		fmt.Println(curPkg.Name)
+		p, _ := npmPkg.GetDependencies(curPkg.Name, curPkg.Version)
+		unprocessed := refreshDependencies(flatDependencies, p.Dependencies)
+		pkgsToProcess = append(pkgsToProcess, unprocessed...)
 	}
 	return flatDependencies, nil
 }
 
-func addMap(bigMap, smallMap map[string]string) {
-	for k, v := range smallMap {
-		bigMap[k] = v
+func refreshDependencies(bigMap, smallMap map[string]string) []pkgToProcess {
+	var unprocessed []pkgToProcess
+	for name, val := range smallMap {
+		if _, ok := bigMap[name]; !ok {
+			pkg := pkgToProcess{
+				Name: name,
+				Version: val,
+			}
+			unprocessed = append(unprocessed, pkg)
+		}
+		bigMap[name] = val
 	}
+	return unprocessed
 }
